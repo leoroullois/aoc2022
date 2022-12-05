c769d388dc987baad69d79d3aca2b74c8755103d
leoroullois <roullois.leo@protonmail.com>
2022-12-05 16:24:27 +0100

    node types

--- day5/index.ts
+++ day5/index.ts
@@ -1,5 +1,5 @@
-const path = require("path");
-const fs = require("fs");
+import path from "path";
+import fs from "fs";
 
 const input = fs
 	.readFileSync(path.join(__dirname, "input.txt"), "utf8")
